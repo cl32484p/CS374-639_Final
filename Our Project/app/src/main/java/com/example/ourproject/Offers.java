8aeb42b954efd8bb21d6a0a4b10357e10262dffc
FreddyAmgad <FreddyAmgad@aucegypt.edu>
2023-05-11 15:55:58 -0400

    adding lay out

---
+++ Our Project/app/src/main/java/com/example/ourproject/Offers.java
@@ -0,0 +1,13 @@
+package com.example.ourproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Offers extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_offers);
+    }
+}
