270bce92173ef8a61cd58a622c001fe1b8fa2a71
Ceasar <cl32484p@pace.edu>
2023-05-12 08:19:34 -0400

    adding Freddy's offers work

--- Our Project/app/src/main/java/com/example/ourproject/Home.java
+++ Our Project/app/src/main/java/com/example/ourproject/Home.java
@@ -1,29 +1,42 @@
 package com.example.ourproject;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-import android.widget.TextView;
-
-import java.util.Date;
-import java.util.concurrent.TimeUnit;
-
 public class Home extends AppCompatActivity {
 
-    //This activity will be the main hub for the user and our app
-
-    private TextView todayView, endView;
+    private TextView balanceTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+        Button calorieCalculatorButton = findViewById(R.id.calorieCalculatorButton);
+        calorieCalculatorButton.setOnClickListener(view -> {
+            Intent intent = new Intent(Home.this, CalorieCalculatorActivity.class);
+            startActivity(intent);
+        });
 
+        Button offersButton = findViewById(R.id.offersButton);
+        offersButton.setOnClickListener(view -> {
+            Intent intent = new Intent(Home.this, Login.class);
+            startActivity(intent);
+        });
 
+        Button transactionsButton = findViewById(R.id.transactionsButton);
+        transactionsButton.setOnClickListener(view -> {
+            Intent intent = new Intent(Home.this, editBalance.class);
+            startActivity(intent);
+        });
 
-
-
-
+        Button profileButton = findViewById(R.id.profileButton);
+        profileButton.setOnClickListener(view -> {
+            Intent intent = new Intent(Home.this, CalorieCalculatorActivity.class);
+            startActivity(intent);
+        });
     }
 }
