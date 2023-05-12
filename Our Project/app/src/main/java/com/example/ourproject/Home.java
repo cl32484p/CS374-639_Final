9971e871194f18dbf77eacfa0abc2806f3184f3e
Ceasar <cl32484p@pace.edu>
2023-05-12 03:44:24 -0400

    Completed functionality of editBalanace page, added the work of Freddy and Shakti(Offers and Calorie Page)

--- Our Project/app/src/main/java/com/example/ourproject/Home.java
+++ Our Project/app/src/main/java/com/example/ourproject/Home.java
@@ -3,15 +3,27 @@ package com.example.ourproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
 public class Home extends AppCompatActivity {
 
     //This activity will be the main hub for the user and our app
 
+    private TextView todayView, endView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+
+
+
+
+
+
     }
 }
